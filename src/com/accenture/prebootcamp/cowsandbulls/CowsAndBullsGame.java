d1c1256439e18cea9f650556b8a857215a244e8a
Šarlote Kumpiņa <sarlote.kumpina@gmail.com>
2020-09-17 22:16:35 +0300

    Hei, Mazais :))

---
+++ src/com/accenture/prebootcamp/cowsandbulls/CowsAndBullsGame.java
@@ -0,0 +1,8 @@
+package com.accenture.prebootcamp.cowsandbulls;
+
+public class CowsAndBullsGame {
+    public static void main(String[] args) {
+        System.out.println("Hello, World :]");
+        System.out.println("Hei, Mazais :))");
+    }
+}
