26e91174a50027f4092171898e0c87fea2665645
LauraDobele <LauraDobele@users.noreply.github.com>
2020-09-21 17:43:12 +0300

    Small changes before lecture.

--- src/com/accenture/prebootcamp/cowsandbulls/CowsAndBullsGame.java
+++ src/com/accenture/prebootcamp/cowsandbulls/CowsAndBullsGame.java
@@ -1,8 +1,22 @@
 package com.accenture.prebootcamp.cowsandbulls;
 
 public class CowsAndBullsGame {
+/*
+BURN AFTER READING
+
+*Klau, es te nedaudz paštukoju par to, kā būvēt sākumu un mans piedāvājums ir šāds.
+Varēsim izrunāt lekcijas laikā, ja kaut kas ir nesaprotams... Protams, varam mainīt, ja vēlies savādāk!
+*Nomainīju nosaukumu no "UserGuess" uz "Utils", jo tajā klasē principā glabāsies visas metodes utt utt
+*Un vēl viena lieta - man šķiet, ka tur vajag tomēr ciparus ģenerēt no 1-9, ne 0-9. Tāpēc piedāvāju ģenerētos
+ciparus salīdzināt ar String, kurā ir jau ielikta arī tā nulle (tas tā, lai, ja random uzģenerē 0, tad
+ģenerē citu skaitli).. Pagaidām atstāju "public" un uzrakstīju to "for" ciklu drukāšanai, lai redzētu, kā strādā ģenerators.
+ */
+
     public static void main(String[] args) {
-        System.out.println("Hello, World :]");
-        System.out.println("Hei, Mazais :))");
+        int[] randomNumber = Utils.generateNumber();
+
+        for (int i = 0; i < randomNumber.length; i++) {
+            System.out.print(randomNumber[i] + " ");
+        }
     }
 }
---
+++ src/com/accenture/prebootcamp/cowsandbulls/Utils.java
@@ -0,0 +1,23 @@
+package com.accenture.prebootcamp.cowsandbulls;
+
+import java.util.Random;
+
+public class Utils {
+
+    public static int[] generateNumber() {
+
+        Random random = new Random();
+        int[] myArray = new int[4];
+        String memory = "0";
+
+        for (int i = 0; i < 4; i++) {
+            int number = random.nextInt(9);
+            while (memory.contains(Integer.toString(number))) {
+                number = random.nextInt(9);
+            }
+            myArray[i] = number;
+            memory += number;
+        }
+        return myArray;
+    }
+}
