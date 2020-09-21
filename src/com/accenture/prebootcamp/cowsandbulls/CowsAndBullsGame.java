80fc9771bbf4292b2fc9a7f60404e9688820a802
Šarlote Kumpiņa <sarlote.kumpina@gmail.com>
2020-09-21 21:56:51 +0300

    updated cow count

--- src/com/accenture/prebootcamp/cowsandbulls/CowsAndBullsGame.java
+++ src/com/accenture/prebootcamp/cowsandbulls/CowsAndBullsGame.java
@@ -17,10 +17,10 @@ public class CowsAndBullsGame {
         String numbersOnly = userString.trim();
         System.out.println("Your guess: " + numbersOnly);
 
-        //String[] usersArray = numbersOnly.split("");
-        //for (int i = 0; i< usersArray.length; i++){
-        //    System.out.print(usersArray[i] + " ");
-        // }
+//        String[] usersArray = numbersOnly.split("");
+//        for (int i = 0; i< usersArray.length; i++){
+//            System.out.print(usersArray[i] + " ");
+//         }
 
         int[] userNumber = new int[4];
         for (int i = 0; i < numbersOnly.length(); i++) {
@@ -30,41 +30,31 @@ public class CowsAndBullsGame {
 
         int bullsCount = 0;
         for (int i = 0; i < userNumber.length; i++) {
-//            int number = userNumber[i];
             if (randomNumber[i] == userNumber[i]) {
                 bullsCount += 1;
             }
         }
 
         int cowsCount = 0;
-        boolean exists = false;
-        for (int i = 0; i < userNumber.length; i++) {
-            int number = randomNumber[i];
-                    if (number == userNumber[i]) {
-                        exists = true;
-                        cowsCount += 1;
-                    }
+        for (int i = 0; i < userNumber.length; ++i) {
+//            System.out.println("Outer loop iteration " + i);
+            for (int j = 0; j < randomNumber.length; ++j) {
+                if (userNumber[i] == randomNumber[j]) {
+                    cowsCount += 1;
                 }
+                //System.out.println("usersNumber[0] = " + userNumber[i] + "; randomNumber[j] = " + randomNumber[j] + " AND cows count is - " + cowsCount);
+            }
+        }
 
-//        do {
-//            int userGuess = userInput.nextInt();
-//            boolean exists = false;
-//            for (int i = 0; i < myList.length; i++) {
-//                if (userGuess == myList[i]) {
-//                    exists = true;
-//                }
-//            }
-//
-
-        System.out.print("Bulls - " + bullsCount + " Cows - " + cowsCount);
+       System.out.print("Bulls - " + bullsCount + " Cows - " + cowsCount);
 
 
         System.out.println();
 
 
-        for (int x = 0; x < randomNumber.length; x++) {
+        for (int i = 0; i < randomNumber.length; i++) {
 
-            System.out.print(randomNumber[x] + " ");
+            System.out.print(randomNumber[i] + " ");
         }
 
 
